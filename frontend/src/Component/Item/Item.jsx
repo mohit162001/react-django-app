fe28f0927e964ed33fcc83e4429af8d03cb70272
Mohit Devade <mohit.deavade123@gmail.com>
2024-03-16 19:08:48 +0530

    item comp updated

--- frontend/src/Component/Item/Item.jsx
+++ frontend/src/Component/Item/Item.jsx
@@ -5,15 +5,15 @@ const Item = (props) => {
 
   return (
     <div className="item">
-        <Link to={`/product/${props.id}`} ><img onClick={window.scrollTo(0,0)} src={"http://localhost:1337"+props.image} alt={props.name} /></Link>
+        <Link to={`/product/${props.id}`} ><img onClick={window.scrollTo(0,0)} src={"http://localhost:8000/media/"+props.image} alt={props.name} /></Link>
         <p>{props.name}</p>
         <div className="item-prices">
           <div className="item-price-new">
           ₹{props.new_price}
           </div>
-          <div className="item-price-old">
+          {/* <div className="item-price-old">
           ₹{props.old_price}
-          </div>
+          </div> */}
         </div>
     </div>
   )
