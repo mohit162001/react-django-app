fba07d68e61d32100c5650b195d795fd9aa74706
Mohit Devade <mohit.deavade123@gmail.com>
2024-03-16 19:08:19 +0530

    add product display page, popular product comp,related porducts

--- frontend/src/Component/Popular/Popular.jsx
+++ frontend/src/Component/Popular/Popular.jsx
@@ -3,9 +3,11 @@ import './popular.css'
 // import data_product from '../Assests/data';
 import Item from '../Item/Item';
 import { useQuery } from '@apollo/client';
-import { GET_POPULAR_PRODUCT } from '../../query/query';
+import { GET_POPULAR_PRODUCT, GET_PRODUCTS_BY_CATEGORY } from '../../query/query';
 const Popular = () => {
-  const {data,error,loading} = useQuery(GET_POPULAR_PRODUCT)
+  const {data,error,loading} = useQuery(GET_PRODUCTS_BY_CATEGORY,{
+    variables: { categoryId: '2' }
+  })
   return (
     <div className='popular'>
         <h1>POPULAR IN WOMEN</h1>
@@ -14,14 +16,14 @@ const Popular = () => {
         {error && <p>Something went wrong</p>}
         {loading && !error && <p>Loading Popular Products...</p>}
         {data && !loading && <div className="popular-item">
-            {data.products.data.map((item,i)=>{
+            {data.productByCategory.map((item,i)=>{
                 return <Item
                 key={i}
                 id={item.id}
-                name={item.attributes.name}
-                image={item.attributes.image.data.attributes.url}
-                new_price={item.attributes.new_price}
-                old_price={item.attributes.old_price}
+                name={item.name}
+                image={item.image}
+                new_price={item.price}
+                old_price={0}
               />
             })}
         </div>}
--- frontend/src/Component/RelatedProducts/RelatedProduct.jsx
+++ frontend/src/Component/RelatedProducts/RelatedProduct.jsx
@@ -6,7 +6,7 @@ import { useQuery } from '@apollo/client'
 import { GET_PRODUCTS_BY_CATEGORY } from '../../query/query'
 const RelatedProduct = ({category}) => {
   const {data} = useQuery(GET_PRODUCTS_BY_CATEGORY,{variables:{
-    category_name:category
+    categoryId:category
   }})
   return (
     <div className="relatedproduct">
@@ -14,15 +14,15 @@ const RelatedProduct = ({category}) => {
         <hr />
         {data && 
         <div className="relatedproduct-item">
-            {data.products.data.slice(0, 4).map((item, i) => {
+            {data.productByCategory.slice(1, 4).map((item, i) => {
               return (
                 <Item
                   key={i}
                   id={item.id}
-                  name={item.attributes.name}
-                  image={item.attributes.image.data.attributes.url}
-                  new_price={item.attributes.new_price}
-                  old_price={item.attributes.old_price}
+                  name={item.name}
+                  image={item.image}
+                  new_price={item.price}
+                  // old_price={item.attributes.old_price}
                 />
               );
           })}
