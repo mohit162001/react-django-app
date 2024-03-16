8f2133cabe8b8f3153da05147de7461d2a4631a6
Mohit Devade <mohit.deavade123@gmail.com>
2024-03-16 19:06:10 +0530

    add cartitem component

--- frontend/src/Component/CartItems/CartItems.jsx
+++ frontend/src/Component/CartItems/CartItems.jsx
@@ -3,9 +3,28 @@ import './cartitems.css'
 import { ShopContext } from '../../Context/ShowContext'
 import remove_icon from '../Assests/cart_cross_icon.png'
 import { useMutation } from '@apollo/client'
-const CartItems = ({products}) => {
+import { REMOVE_CART_ITEM } from '../../query/query'
+import { checkAuth, getUserData } from '../../helper'
+const CartItems = ({products,refetch}) => {
     const {getTotalCartAmount,all_product,cartItems,removeFromCart} = useContext(ShopContext)
-    // const [mutationFun] = useMutation()
+    const [mutationFun] = useMutation(REMOVE_CART_ITEM)
+    const userId = getUserData("id");
+    function getTotalAmount(){
+        const total= products.cart.reduce((acc,cur)=>(acc+cur.price),0)
+        return total
+     }
+     function removeItem(id) {
+        if (checkAuth() === true) {
+          mutationFun({
+            variables: {
+              userId: userId,
+              productId: id,
+            },  
+          });
+        }
+        refetch()
+     }
+
     return (
     <div className="cartitems">
         <div className="cartitems-format-main">
@@ -17,15 +36,15 @@ const CartItems = ({products}) => {
             <p>Remove</p>
         </div>
         <hr />
-        {products.map((item)=>{
+        {products.cart.map((item)=>{
                 return <div key={item.id}>
                             <div className="cartitems-format cartitems-format-main">
-                                <img src={"http://localhost:1337"+item.attributes.image.data.attributes.url} alt="" className='carticon-product-icon' />
-                                <p>{item.attributes.name}</p>
-                                <p> ₹{item.attributes.new_price}</p>
-                                <button className='cartitems-quantity'>{cartItems[item.attributes.id]}</button>
-                                <p> ₹{item.attributes.new_price * cartItems[item.attributes.id]}</p>
-                                <img src={remove_icon} onClick={()=>{removeFromCart(item.id)}} alt="" />
+                                <img src={"http://localhost:8000/media/"+item.product.image} alt="" className='carticon-product-icon' />
+                                <p>{item.product.name}</p>
+                                <p> ₹{item.product.price}</p>
+                                <button className='cartitems-quantity'>{item.quantity}</button>
+                                <p> ₹{item.price}</p>
+                                <img src={remove_icon} onClick={()=>{removeItem(item.product.id)}} alt="" />
                             </div>
                             <hr />
                         </div>
@@ -34,10 +53,10 @@ const CartItems = ({products}) => {
         <div className="cartitems-down">
             <div className="cartitems-total">
                 <h1>Cart Totals</h1>
-                <div>
+                <div className='cartitem-total-container'>
                     <div className="cartitems-total-item">
                         <p>Subtotal</p>
-                        <p> ₹{getTotalCartAmount()}</p>
+                        <p> ₹{getTotalAmount()}</p>
                     </div>
                     <hr />
                     <div className="cartitems-total-item">
@@ -47,18 +66,18 @@ const CartItems = ({products}) => {
                     <hr />
                     <div className="cartitems-total-item">
                         <h3>Total</h3>
-                        <h3> ₹{getTotalCartAmount()}</h3>
+                        <h3> ₹{getTotalAmount()}</h3>
                     </div>
                 </div>
-                <button>PROCEED TO CHECKOUT</button>
+                <button>PROCEED TO ORDER</button>
             </div>
-            <div className="cartitems-promocode">
+            {/* <div className="cartitems-promocode">
                 <p>If you have promocode, Enter here</p>
                 <div className="cartitems-promobox">
                     <input type="text" placeholder='Promocode' />
                     <button type='submit'>Apply</button>
                 </div>
-            </div>
+            </div> */}
         </div>
     </div>
   )
