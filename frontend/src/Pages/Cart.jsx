328533a06430ad60107fcda4f2f1fa45848d2b31
Mohit Devade <mohit.deavade123@gmail.com>
2024-03-18 15:41:30 +0530

    fall back added to cart page

--- frontend/src/Pages/Cart.jsx
+++ frontend/src/Pages/Cart.jsx
@@ -3,22 +3,24 @@ import CartItems from '../Component/CartItems/CartItems';
 import { useQuery } from '@apollo/client';
 import { GET_CART_DETAILS } from '../query/query';
 import { checkAuth, getUserData } from '../helper';
-
+import FallBack from '../Component/FallBack/FallBack';
+import empty_cart from '../Component/Assests/empty-cart.png'
 export const Cart = () => {
-  const id = getUserData('id')
+  const userId = getUserData('id')
 
-  const {data,refetch} = useQuery(GET_CART_DETAILS,{
+  const {data,loading,error,refetch} = useQuery(GET_CART_DETAILS,{
     variables:{
-      userId:id
+      userId:userId
     }
   })
-  useEffect(()=>{
-    refetch()
-  })
+
   return (
   <>
-    {data && <CartItems products = {data} refetch={refetch}/>}
-   </>
+  {error && <p className='product-fallback'>Something went wrong...!</p>}
+  {loading && !error && <p className='product-fallback'>Loading cart details...</p>}
+  {!error && data && data.cart.length>0 && <CartItems products={data} refetch={refetch} userId={userId}/>}
+  {!error && data && data.cart.length===0 && <FallBack image={empty_cart} heading={"No item found"} btn_lable={"Add item"} setMenuValue={'mens'} link={'/mens'}/>} 
+  </>
   )
 }
 export default Cart;
