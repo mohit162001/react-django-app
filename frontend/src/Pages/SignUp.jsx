b20f750a74963fbc9d5b6ff0045555a8f0e9348a
Mohit Devade <mohit.deavade123@gmail.com>
2024-03-22 16:42:15 +0530

    signup functionality added

--- frontend/src/Pages/SignUp.jsx
+++ frontend/src/Pages/SignUp.jsx
@@ -3,42 +3,44 @@ import './CSS/signup.css'
 import { Link, useNavigate } from 'react-router-dom';
 import toast, { Toaster } from 'react-hot-toast';
 import { storeData } from '../helper';
-import { USER_REGISTRATION } from '../query/query';
+import { USER_SIGNUP } from '../query/query';
 import { useMutation } from '@apollo/client';
 import back_icon from '../Component/Assests/back.png'
 
 export const SignUp = () => {
-  // const navigate = useNavigate()
-  // const [mutationFun] = useMutation(USER_REGISTRATION,{
-  //   onCompleted(data){
-  //     toast('Sign Up successfull',{icon:"😊",duration:1000})
-  //     storeData(data.register)
-  //     setTimeout(()=>{
-  //       navigate('/')
-  //     },1000)
-  //   },
-  //   onError(){
-  //     toast.error('Someting went wrong...!',{duration:1000})
-  //   }
-  // })
-  // function handleSubmit(event){
-  //   event.preventDefault()
-  //   const formData = new FormData(event.target)
+  const navigate = useNavigate()
+  const [mutationCreateUser] = useMutation(USER_SIGNUP,{
+    onCompleted(data){
+      toast('Sign Up successfull',{icon:"😊",duration:1000})
+      storeData(data.createUser)
+      console.log("new user----",data)
+      setTimeout(()=>{ 
+        navigate('/')
+      },1000)
+    },
+    onError(){
+      toast.error('Someting went wrong...!',{duration:1000})
+    }
+  })
+  function handleSubmit(event){
+    event.preventDefault()
+    const formData = new FormData(event.target)
     
-  //     const username = formData.get("username")
-  //     const email = formData.get("email")
-  //     const password = formData.get("password")
-    
-  //   if((username&&username.trim().length !==0) && (password && password.trim().length !==0) && (email&&email.trim().length !==0)){
-  //     mutationFun({variables:{
-  //       username,
-  //       password,
-  //       email
-  //     }})
-  //   }else if(email.includes('.com') || password.trim().length<6 || username.trim().length !==0){
-  //     toast(' Enter valid input!',{icon:"⚠️",duration:1000})
-  //   }
-  // }
+      const username = formData.get("username")
+      const email = formData.get("email")
+      const password = formData.get("password")
+    if((username&&username.trim().length !==0) && (password && password.trim().length !==0) && (email&&email.trim().length !==0)){
+      console.log(username,email,password)
+      
+      mutationCreateUser({variables:{
+        username,
+        password,
+        email
+      }})
+    }else if(email.includes('.com') || password.trim().length<6 || username.trim().length !==0){
+      toast(' Enter valid input!',{icon:"⚠️",duration:1000})
+    }
+  }
   return (
     <>
     <Toaster/>
@@ -46,16 +48,12 @@ export const SignUp = () => {
     <Link to='/'><button className='back-btn'><img src={back_icon} alt="" />Back</button></Link>
       <div className="signup-container">
         <h1>Sign Up</h1>
-        <form action="" onSubmit={"handleSubmit"} >
+        <form action="" onSubmit={handleSubmit} >
         <div className="signup-fields">
-          <input type="text" placeholder='Your Name' name='username' required />
-          <input type="email" placeholder='Email Adress' name='email' required />
-          <input type="password" placeholder='Password' name='password' required />
+          <input type="text" placeholder='Your Name' name='username'  />
+          <input type="email" placeholder='Email Adress' name='email'  />
+          <input type="password" placeholder='Password' name='password'  />
         </div>
-        {/* <div className="signup-agree">
-          <input type="checkbox" name='' id='' />
-          <p>By continuing I agree to the terms and conditions</p>
-        </div> */}
         <button type='submit'>Continue</button>
         </form>
         <p className='signup-login'>Already have an account? <span><Link to='/login'>Login</Link></span></p>
