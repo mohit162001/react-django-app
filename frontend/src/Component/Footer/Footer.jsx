fa11d558b1ad7c2fb1ce1c75ac1ec85706f3488c
Mohit Devade <mohit.deavade123@gmail.com>
2024-03-23 18:52:49 +0530

    conditional rendering added

--- frontend/src/Component/Footer/Footer.jsx
+++ frontend/src/Component/Footer/Footer.jsx
@@ -4,6 +4,7 @@ import footer_logo from "../Assests/logo_big.png";
 import instagran_icon from "../Assests/instagram_icon.png";
 import facebook_icon from "../Assests/facebook_icon.png";
 import whatsapp_icon from "../Assests/whatsapp_icon.png";
+import { isAdminUser } from "../../helper";
 const Footer = () => {
   return (
     <div className="footer">
@@ -11,14 +12,14 @@ const Footer = () => {
         <img src={footer_logo} alt="" />
         <p>CLOTHY</p>
       </div> */}
-      <ul className="footer-links">
+      {!isAdminUser()&&<ul className="footer-links">
         <li>Contact</li>
         <li>About</li>
         <li>Services</li>
         {/* <li>Products</li> */}
         {/* <li>Offices</li> */}
-      </ul>
-      <div className="footer-social-icon">
+      </ul>}
+      {!isAdminUser() && <div className="footer-social-icon">
         <div className="footer-icons-container">
           <img src={instagran_icon} alt="" />
         </div>
@@ -28,7 +29,7 @@ const Footer = () => {
         <div className="footer-icons-container">
           <img src={whatsapp_icon} alt="" />
         </div>
-      </div>
+      </div>}
       <div className="footer-copyright">
         <hr />
         <p>Copyright @2024 - All Right Reserved by DYNAMIC</p>
