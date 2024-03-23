85b374a78c5e23212fc0eb1908765c1c32691198
Mohit Devade <mohit.deavade123@gmail.com>
2024-03-23 18:51:40 +0530

    function import changes

--- frontend/src/Component/Navbar/Navbar.jsx
+++ frontend/src/Component/Navbar/Navbar.jsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import './navbar.css'
 import logo from '../Assests/logo.png'
 import cart_icon from '../Assests/cart_icon.png'
 import { Link, useNavigate } from 'react-router-dom';
 import { useContext } from 'react';
 import { ShopContext } from '../../Context/ShowContext';
-import { checkAuth, clearData, getUserData } from '../../helper';
+import {  clearData, getUserData, isAdminUser } from '../../helper';
 import { useQuery } from '@apollo/client';
 import { GET_CART_DETAILS } from '../../query/query';
 import avatar from '../Assests/avatar.png'
@@ -42,15 +42,26 @@ function Navbar({}) {
             <p><Link onClick={()=>setMenu('shop')}>CLOTHY</Link></p>
         </div>
         <ul className="nav-menu">
-            <li onClick={()=>{setMenu('shop')}}><Link style={{textDecoration:'none'}} to='/'>Shop</Link>{menu==='shop'? <hr/>:<></>} </li>
+            {!isAdminUser() && <>
+              <li onClick={()=>{setMenu('shop')}}><Link style={{textDecoration:'none'}} to='/'>Shop</Link>{menu==='shop'? <hr/>:<></>} </li>
             <li onClick={()=>{setMenu('mens')}}><Link style={{textDecoration:'none'}} to='/mens'>Mens</Link>{menu==='mens' ? <hr/>:<></>}</li>
             <li onClick={()=>{setMenu('womens')}}><Link style={{textDecoration:'none'}} to='/womens'>Womens</Link>{menu==='womens' ? <hr/>:<></>}</li>
             <li onClick={()=>{setMenu('order')}}><Link style={{textDecoration:'none'}} to='/order'>My orders</Link>{menu==='order' ? <hr/>:<></>} </li>
+            </>}
+            {isAdminUser() && <>
+            <li onClick={()=>{setMenu('shop')}}><Link style={{textDecoration:'none'}} to='/'>Shop</Link>{menu==='shop'? <hr/>:<></>} </li>
+            <li onClick={()=>{setMenu('allproduct')}}><Link style={{textDecoration:'none'}} to='/admin/allproducts'>All products</Link>{menu==='allproduct' ? <hr/>:<></>}</li>
+            <li onClick={()=>{setMenu('allorders')}}><Link style={{textDecoration:'none'}} to='/admin/allorders'>All Orders</Link>{menu==='allorders' ? <hr/>:<></>}</li>
+            <li onClick={()=>{setMenu('addproduct')}}><Link style={{textDecoration:'none'}} to='/admin/addproduct'>Add Product</Link>{menu==='addproduct' ? <hr/>:<></>} </li>
+            <li onClick={()=>{setMenu('addcategory')}}><Link style={{textDecoration:'none'}} to='/admin/addcategory'>Add Category</Link>{menu==='addcategory' ? <hr/>:<></>} </li>
+            </>}
         </ul>
         <div className="nav-login-cart">
+            {!isAdminUser() &&<>
             <Link to='/cart' onClick={()=>{setMenu('cart')}}><img src={cart_icon} alt="" /></Link>
             <div className="nav-cart-count">{cartValue}</div>
-            {token && <div className='nav-avatar'><Link to='user'><img onClick={()=>{setMenu('')}} className='nav-avatar-img' src={avatar} alt="" /></Link></div>}
+            </>}
+            {token && <div className='nav-avatar'><Link to='user'><img onClick={()=>{setMenu('')}} className='nav-avatar-img' src={isAdminUser()?admin_image1:avatar} alt="" /></Link></div>}
             {token? <Link onClick={handleLogOut}><button>Log out</button></Link>: <Link to='/login'><button>Login</button></Link>}
         </div>
     </div>
