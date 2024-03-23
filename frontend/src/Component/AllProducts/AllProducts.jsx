8a841d85f33cd7dd57425a5435f3b6b8009bb067
Mohit Devade <mohit.deavade123@gmail.com>
2024-03-23 18:49:41 +0530

    all products page added

---
+++ frontend/src/Component/AllProducts/AllProducts.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import './allproducts.css';
+import { useMutation } from '@apollo/client';
+import { DELETE_PRODUCT, GET_ALL_PRODUCTS } from '../../query/query';
+import toast, { Toaster } from 'react-hot-toast';
+import delete_icon from '../Assests/delete-icon.png'
+import edit_icon from '../Assests/edit-icon.png'
+import { Link } from 'react-router-dom';
+
+function AllProducts({ products, currPage, itemsperPage }) {
+
+  const start = (currPage - 1) * itemsperPage;
+  const end = start + itemsperPage;
+
+  const [mutationProductDelete,{data}] = useMutation(DELETE_PRODUCT,{
+    onCompleted(){
+        toast.success("Product deleted successfully",{duration:1000})
+    },
+    onError(){
+        toast.error("Can not delete ")
+    },
+    refetchQueries: [{ query: GET_ALL_PRODUCTS }]
+  })
+
+  function handleDelete(productId){
+    console.log(productId)
+    // mutationProductDelete({
+    //     variables:{
+    //         productId:productId
+    //     }
+    // })
+  }
+  return (
+    <>
+    <Toaster/>
+      <div className="allproduct">
+        <div className="allproduct-format-main">
+          <p>Product</p>
+          <p>Title</p>
+          <p>Price</p>
+          {/* <p>Descrtiption</p> */}
+          <p>Created at</p>
+          <p>Category</p>
+          <p>Action</p>
+        </div>
+        <hr className='allproduct-hr'/>
+        <div className='allproduct-list-container'>
+        {products.slice(start, end).map((item,i) => {
+          return (
+            <div  key={i}>
+              <div className="allproduct-format allproduct-format-main">
+                <Link to={'/product/'+item.node.id}><img src={"http://localhost:8000/media/" + item.node.image} alt="" className='carticon-product-icon' /></Link>
+                <p className='allproduct-p'>{item.node.name}</p>
+                <p className='allproduct-p'>₹{item.node.price}</p>
+                {/* <p> {item.node.desc}</p> */}
+                <p > {item.node.insertedDate}</p>
+                <p className='allproduct-p'> {item.node.category.name}</p>
+                <div className='allproduct-action'>
+                <Link to={'/admin/addproduct/'+item.node.id}><img src={edit_icon} className='allproduct-action-btn'/></Link>
+                <img src={delete_icon} onClick={()=>handleDelete(item.node.id)} className='allproduct-action-btn'/>
+                </div>
+              </div>
+              <hr className='allproduct-hr'/>
+            </div>
+          );
+        })}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default AllProducts;
---
+++ frontend/src/Pages/AllProductsPage.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import FallBack from '../Component/FallBack/FallBack';
+import { useQuery } from '@apollo/client';
+import { GET_ALL_ORDERS, GET_ALL_PRODUCTS } from '../query/query';
+import empty_order from '../Component/Assests/empty-order.png';
+import right_arrow from '../Component/Assests/breadcrum_arrow.png'
+import AllProducts from '../Component/AllProducts/AllProducts';
+function AllProductsPage() {
+    const {data,loading,error} = useQuery(GET_ALL_PRODUCTS)
+    const [currPage, setCurrPage] = useState(1);
+    console.log(data)
+    const totalPages = Math.ceil((data && data.products.edges.length)/4)
+    // console.log("total item",data && data.products.edges.length)
+    // console.log("total pages",totalPages)
+    // console.log("curr page",currPage)
+    const nextPage = () => {
+      setCurrPage((prevPage) => prevPage + 1);
+    };
+  
+    const prevPage = () => {
+      setCurrPage((prevPage) => prevPage - 1);
+    };
+    return (
+        <>
+          {error && <p className='product-fallback'>Something went wrong...!</p>}
+          {loading && !error && <p className='product-fallback'>Loading products details...</p>}
+          {!error && data && data.products.edges.length > 0 && <>
+              <div>
+              <AllProducts products={data.products.edges} currPage={currPage} itemsperPage={4} />
+              <div className="pagination">
+                <button className={currPage===1?'disabled-btn':'pagination-btn'} disabled={currPage===1?true:false} onClick={prevPage} >  <img className='left-arrow' src={right_arrow} alt="" />Previous</button>
+                <button className={currPage===totalPages?'disabled-btn':'pagination-btn'} disabled={currPage===totalPages?true:false} onClick={nextPage} >Next <img className='rigth-arrow' src={right_arrow} alt="" /> </button>
+              </div>
+              </div>
+            </>
+          }
+          {!error && data && data.products.edges.length === 0 && (<FallBack image={empty_order} heading={"No order found"} btn_lable={"Order now"} setMenuValue={'shop'} link={'/'}/>
+          )}
+        </>
+      );
+}
+
+export default AllProductsPage
