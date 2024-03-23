d7b556eb30e8ce12fb1673dbb6e7df45d9b3f65f
Mohit Devade <mohit.deavade123@gmail.com>
2024-03-23 18:51:14 +0530

    admin authorization added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ import men_banner from "./Component/Assests/banner_mens.png";
 import women_banner from "./Component/Assests/banner_women.png";
 // import kid_banner from "./Component/Assests/banner_kids.png";
 import RootLayOut from "./Pages/RootLayOut";
-import { checkAuth } from "./helper";
+import { isAdmin, isAuthenticated } from "./helper";
 import Login from "./Pages/LogIn";
 import Orders from "./Pages/Orders";
 
@@ -19,6 +19,7 @@ import AdminLayout from "./Pages/AdminLayout";
 import AddProductPage from "./Pages/AddProductPage";
 import AddCategoryPage from "./Pages/AddCategoryPage";
 import AllOrdersPage from "./Pages/AllOrdersPage";
+import AllProductsPage from "./Pages/AllProductsPage";
 
 
 function App() {
@@ -44,13 +45,15 @@ function App() {
           element: <Product />,
           children: [{ path: ":productId", element: <Product /> }],
         },
-        { path: "/order", element: <Orders/>,loader:checkAuth },
-        { path: "/cart", element: <Cart />,loader:checkAuth },
-        { path: "/user", element: <UserProfilePage/>,loader:checkAuth},
-        {path:"/admin",element:<AdminLayout/>,children:[
+        { path: "/order", element: <Orders/>,loader:isAuthenticated },
+        { path: "/cart", element: <Cart />,loader:isAuthenticated },
+        { path: "/user", element: <UserProfilePage/>,loader:isAuthenticated},
+        {path:"/admin",element:<AdminLayout/>,loader:isAdmin,children:[
           {path:'addproduct',element:<AddProductPage/>},
+          {path:'addproduct/:productId',element:<AddProductPage/>},
           {path:'addcategory',element:<AddCategoryPage/>},
-          {path:'allorders',element:<AllOrdersPage/>}
+          {path:'allorders',element:<AllOrdersPage/>},
+          {path:'allproducts',element:<AllProductsPage/>},
         ]}
       ],
     }, { path: "signup", element: <SignUp /> },
