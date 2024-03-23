ea3147cdcca78794edfc866041b357f2f6b6a61c
Mohit Devade <mohit.deavade123@gmail.com>
2024-03-23 18:47:53 +0530

    order deletion added

--- frontend/src/Component/AllOrderItems/AllOrderItems.jsx
+++ frontend/src/Component/AllOrderItems/AllOrderItems.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
 import './allorderitems.css';
+import delete_icon from '../Assests/delete-icon.png'
+import { useMutation } from '@apollo/client';
+import {DELETE_ORDER, GET_ALL_ORDERS} from '../../query/query'
+import toast, { Toaster } from 'react-hot-toast';
 
 function AllOrdersItem({ orders, currPage, itemsperPage }) {
 
   const start = (currPage - 1) * itemsperPage;
   const end = start + itemsperPage;
 
+  const [mutationOrderDelete] = useMutation(DELETE_ORDER,{
+    onCompleted(data){
+      toast.success("Order delete successfully")
+    },
+    onError(error){
+      toast.error("Something went wrong")
+    },
+    refetchQueries: [{ query: GET_ALL_ORDERS }]
+  })
+  function handleDelete(orderId){
+    mutationOrderDelete({variables:{
+      orderId:orderId
+    }})
+
+  }
   return (
     <>
+    <Toaster/>
       <div className="allorderitems">
         <div className="allorderitems-format-main">
           <p>Product</p>
@@ -17,6 +37,7 @@ function AllOrdersItem({ orders, currPage, itemsperPage }) {
           <p>Quantity</p>
           <p>Total</p>
           <p>Pay mode</p>
+          <p>Action</p>
         </div>
         <hr />
         <div className='allorder-list-container'>
@@ -31,6 +52,9 @@ function AllOrdersItem({ orders, currPage, itemsperPage }) {
                 <button className='allorderitems-quantity'>{item.quantity}</button>
                 <p> ₹{item.totalPrice}</p>
                 <p> {item.paymentMode}</p>
+                <div className='allorderitems-action'>
+                <img src={delete_icon} onClick={()=>handleDelete(item.orderId)} className='allproduct-action-btn'/>
+                </div>
               </div>
               <hr />
             </div>
--- frontend/src/Component/OrdersItem/OrdersItem.jsx
+++ frontend/src/Component/OrdersItem/OrdersItem.jsx
@@ -1,15 +1,37 @@
 import React from 'react';
 import './ordersitem.css';
+import { DELETE_ORDER, GET_ORDERS_DETAILS } from '../../query/query';
+import { useMutation } from '@apollo/client';
+import toast, { Toaster } from 'react-hot-toast';
+import delete_icon from '../Assests/delete-icon.png'
 
-function OrdersItem({ orders, currPage, itemsperPage }) {
+
+function OrdersItem({ orders, currPage, itemsperPage,userId }) {
 
   const start = (currPage - 1) * itemsperPage;
   const end = start + itemsperPage;
 
+  const [mutationOrderDelete] = useMutation(DELETE_ORDER,{
+    onCompleted(data){
+      toast.success("Order delete successfully",{duration:1000})
+    },
+    onError(error){
+      toast.error("Something went wrong",{duration:1000})
+    },
+    refetchQueries: [{ query: GET_ORDERS_DETAILS,variables:{userId:userId} }]
+  })
+  function handleDelete(orderId){
+    console.log(orderId)
+    mutationOrderDelete({variables:{
+      orderId:orderId
+    }})
+
+  }
 
 
   return (
     <>
+    <Toaster/>
       <div className="orderitems">
         <div className="orderitems-format-main">
           <p>Product</p>
@@ -18,6 +40,7 @@ function OrdersItem({ orders, currPage, itemsperPage }) {
           <p>Quantity</p>
           <p>Total</p>
           <p>Pay mode</p>
+          <p>Action</p>
         </div>
         <hr />
         <div className='order-list-container'>
@@ -31,6 +54,9 @@ function OrdersItem({ orders, currPage, itemsperPage }) {
                 <button className='orderitems-quantity'>{item.quantity}</button>
                 <p> ₹{item.totalPrice}</p>
                 <p> {item.paymentMode}</p>
+                <div className='orderitems-action'>
+                <img src={delete_icon} onClick={()=>handleDelete(item.orderId)} className='orderitems-action-btn'/>
+                </div>
               </div>
               <hr />
             </div>
